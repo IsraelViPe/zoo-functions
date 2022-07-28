d78b18a7ad4bca8f50947aea52f0b422736c70c4
IsraelPereira <pereira.israel2070@gmail.com>
2022-07-28 17:35:45 -0300

    requisito 4, lógica funciona mas estou com problema para passar no test do erro

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,22 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some(({ managers }) => managers.includes(id));
 }
+// Caso a pessoa seja gerente, retorne um array contendo nome e sobrenome das pessoas colaboradoras gerenciadas por essa pessoa.
+// se não for gerernte throw  new  Error('O id inserido não é de uma pessoa colaboradora gerente!');
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return data.employees.reduce((arr, { managers }, index, array) => {
+    // console.log(index, array[index].firstName);
+    if (managers.includes(managerId)) {
+      arr.push(`${array[index].firstName} ${array[index].lastName} `);
+    }
+    return arr;
+  }, []);
 }
-
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 module.exports = { isManager, getRelatedEmployees };
